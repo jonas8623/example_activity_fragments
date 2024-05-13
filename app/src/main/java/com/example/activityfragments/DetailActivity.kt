3033a55a0d3dcc9abb8f318b042ab342f7827f39
jonas felipe quirino <jonas-felipe16@hotmail.com>
2024-05-13 20:02:44 -0300

    initial commit

---
+++ app/src/main/java/com/example/activityfragments/DetailActivity.kt
@@ -0,0 +1,27 @@
+package com.example.activityfragments
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailActivity : AppCompatActivity() {
+
+    lateinit var buttonBack: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        buttonBack = findViewById(R.id.button_back)
+        buttonBack.setOnClickListener { finish() } // Finalizar
+    }
+}
---
+++ app/src/main/java/com/example/activityfragments/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.activityfragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonNewPage: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        buttonNewPage = findViewById(R.id.button_new_page)
+        buttonNewPage.setOnClickListener {
+
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent) // Abrir nova tela
+        }
+    }
+}
